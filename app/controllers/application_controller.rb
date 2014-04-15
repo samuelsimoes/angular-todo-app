7d4607f08cff14209cfaf85657dddc1fa58e9995
Samuel Simões <slip.cm@gmail.com>
2014-04-15 00:37:22 -0300

    Basic app foundation organization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  self.responder = ApplicationResponder
+
+  def javascript_exists?
+    script = "#{Rails.root}/app/assets/javascripts/#{controller_path}.js"
+    File.exists?(script)
+  end
+  helper_method :javascript_exists?
 end
