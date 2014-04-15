db747411d42437f19f94a358af2010d1f452c664
Samuel Simões <slip.cm@gmail.com>
2014-04-15 18:26:05 -0300

    Angular.js base start

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,12 @@
 //= require jquery_ujs
 //= require angular
 //= require angular.resource
-//= require_tree .
+
+angular.module("Todos", []);
+angular.module("TodoApp", ["ngResource", "Todos"]).config([
+  "$httpProvider",
+  function ($httpProvider) {
+    $httpProvider.defaults.headers.common["X-CSRF-Token"] =
+      $("meta[name=csrf-token]").attr("content");
+  }
+]);
